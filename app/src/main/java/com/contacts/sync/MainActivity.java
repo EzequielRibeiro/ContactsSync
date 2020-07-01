53eef480102088bf272b8c950c662eb7020cef9d
Ezequiel <ezequiel.ribeiro@gmail.com>
2020-06-30 21:19:37 -0300

    working

--- app/src/main/java/com/contacts/sync/MainActivity.java
+++ app/src/main/java/com/contacts/sync/MainActivity.java
@@ -1,17 +1,37 @@
 package com.contacts.sync;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private int RC_SIGN_IN = 0;
+    public static final String TAG =  "Contacts Sync";
+    private Button buttonSign;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonSign = findViewById(R.id.buttonSign);
+        buttonSign.setOnClickListener(this);
+
 
         try {
             DriveSync driveSync = new DriveSync(getApplicationContext());
@@ -22,4 +42,55 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+    @Override
+    public void onClick(View view) {
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account,null);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null,e);
+        }
+    }
+
+    private void updateUI(GoogleSignInAccount account, ApiException e){
+
+        if(account != null)
+        Log.w(TAG, "account e-mail=" + account.getEmail());
+        else{
+        Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+        }
+    }
+
+
 }
