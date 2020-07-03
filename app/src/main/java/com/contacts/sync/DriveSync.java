592973d5a818f0661178ab61a4afaf54ef50becf
Ezequiel <ezequiel.ribeiro@gmail.com>
2020-07-03 09:27:51 -0300

    token

--- app/src/main/java/com/contacts/sync/DriveSync.java
+++ app/src/main/java/com/contacts/sync/DriveSync.java
@@ -4,39 +4,31 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.api.client.auth.oauth2.AuthorizationCodeResponseUrl;
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeRequestUrl;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeTokenRequest;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleOAuthConstants;
 import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
 import com.google.api.client.http.FileContent;
-import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
-import com.google.api.client.util.store.MemoryDataStoreFactory;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.drive.model.File;
 import com.google.api.services.drive.model.FileList;
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
+
 
 import static com.contacts.sync.MainActivity.TAG;
 
@@ -48,7 +40,7 @@ public class DriveSync extends AsyncTask<String, Void, Void> {
     private static String TOKENS_DIRECTORY_PATH;
     private Drive service;
     private static GoogleSignInAccount account;
-    private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE);
+    private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE_FILE);
     private static final String CREDENTIALS_FILE_PATH ="" ;
     private static Context context;
     private static GoogleTokenResponse tokenResponse;
@@ -118,7 +110,6 @@ public class DriveSync extends AsyncTask<String, Void, Void> {
                 .setFromTokenResponse(tokenResponse);
 
  */
-
 
       //  return credential;
         Log.w(TAG,"ClientID="+clientSecrets.getDetails().getClientId());
